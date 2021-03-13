854b8aa74ef3b90468243551a88df52ee8cf151f
danilov.v <danilov.personal@gmail.com>
2021-03-13 18:52:51 +0300

    feature login logout

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -6,22 +6,27 @@ import { Hero } from "components/hero";
 import { SearchInput } from "components/search-input";
 import { StyledPageWrapper, StyledContentWrapper } from "./styled";
 import { useSearhPhotos } from "hooks/use-search-photos";
+import { useAuth } from "hooks/use-auth";
 
 export const App = () => {
     const [query, setQuery] = useState("");
     const [page, setPage] = useState(1);
-    const pictures = useSearhPhotos({ query, page });
+    const { photos, refetch } = useSearhPhotos({ query, page });
+    const isAuthorized = useAuth();
 
     return (
         <StyledPageWrapper>
             <StyledContentWrapper>
-                <Header />
+                <Header isAuthorized={isAuthorized} />
                 <Hero>
                     <SearchInput onSearch={setQuery} />
                 </Hero>
                 <GridLayout>
-                    {pictures.map(({ alt_description, urls }) => (
-                        <Card>
+                    {photos.map(({ alt_description, urls }) => (
+                        <Card
+                            isAuthorized={isAuthorized}
+                            onPhotoUpdate={refetch}
+                        >
                             <img
                                 key={alt_description}
                                 alt={alt_description}
